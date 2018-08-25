9ab11423cca8e9e50e5c7b153f9d9ad7ebf38b01
D3-FC <D3-FC@mail.ru>
2018-08-25 13:59:20 +0400

    feat(demo): add demo routes and styles

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//@ts-ignore
+import demoRoutes from './demoRoutes.js'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
+]
+
+const router = new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    }
-  ]
+  routes
 })
+
+
+if (process.env.NODE_ENV === 'development') {
+  //@ts-ignore
+  import('vue-book')
+    .then((VueComponentTree) => {
+      router.addRoutes([
+        VueComponentTree.createRoute({
+          //@ts-ignore
+          requireContext: require.context('./components', true, /.demo.vue$/),
+          path: '/demo',
+          hideFileExtensions: false, // optional, hides file extensions in list.
+        })
+      ])
+    });
+}
+
+export default router
+
