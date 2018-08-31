cfc0c42eab66d33144efd8ed0dd30e7c8b561e2b
D3-FC <D3-FC@mail.ru>
2018-08-31 19:42:21 +0400

    feat(TyshaList):  add theme for color

--- src/components/List/DemoTyshaList.vue
+++ src/components/List/DemoTyshaList.vue
@@ -1,23 +1,49 @@
 <template>
-    <div class="demo-container">
-        <div class="demo-container__item">
-            <tysha-list>
-                <tysha-list-item activated>Item 1</tysha-list-item>
-                <tysha-list-item>Item 2</tysha-list-item>
-                <tysha-list-item>Item 3</tysha-list-item>
-            </tysha-list>
-        </div>
+  <div class="demo-container">
+    <div
+      v-for="(color, i) in colors"
+      :key="i"
+      class="demo-container__item"
+    >
+      <div>
+        {{ color }}
+        <tysha-list
+          :color="color"
+        >
+          <tysha-list-item
+            :activated="selected === 0"
+            @click.native="selected = 0">Item 1</tysha-list-item>
+          <tysha-list-item
+            :activated="selected === 1"
+            @click.native="selected = 1">Item 2</tysha-list-item>
+          <tysha-list-item
+            :activated="selected === 2"
+            @click.native="selected = 2">Item 3</tysha-list-item>
+        </tysha-list>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import TyshaList from './TyshaList.vue'
 import TyshaListItem from './TyshaListItem.vue'
+import { ThemeColor } from '../../enums/ThemeColor'
 
 export default {
   components: {
     TyshaListItem,
     TyshaList
+  },
+  data () {
+    return {
+      selected: 0
+    }
+  },
+  computed: {
+    colors () {
+      return Object.values(ThemeColor)
+    }
   }
 }
 </script>
--- src/components/List/TyshaList.vue
+++ src/components/List/TyshaList.vue
@@ -1,20 +1,49 @@
 <template>
-  <div class="tysha-list">
-    <slot />
+  <div
+    :class="[colorClassList, sizeClassList]"
+    class="tysha-list"
+  >
+    <slot/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { WithColor } from '../../mixins/WithColor'
+import { mixins } from 'vue-class-component'
+import { WithSize } from '../../mixins/WithSize'
 
 @Component
-export default class VList extends Vue {
+export default class TyshaList extends mixins(WithColor, WithSize) {
 
 }
+
 </script>
 
 <style lang="scss">
   .tysha-list {
     background-color: transparent;
+    @include tysha-theme--color('background-color');
+    border-radius: 1px;
+    &--color{
+      &_primary, &_secondary, &_error, &_success,&_info, &_muted{
+        color: $tysha-color--base;
+      }
+    }
+
+    &--size {
+      &_lg {
+        width: $tysha-list__width--lg;
+      }
+      &_md {
+        width: $tysha-list__width--md;
+      }
+      &_sm {
+        width: $tysha-list__width--sm;
+      }
+      &_xs {
+        width: $tysha-list__width--xs;
+      }
+    }
   }
 </style>
