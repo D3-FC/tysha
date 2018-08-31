08a2d6d89a525e72883489c2af861d659bd38dad
D3-FC <D3-FC@mail.ru>
2018-08-31 19:40:43 +0400

    refactor(TyshaText): update demo

--- src/components/Text/DemoTyshaText.vue
+++ src/components/Text/DemoTyshaText.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="demo-container">
-        <div class="demo-container__item">
-            <tysha-text tag="div">
-              in div
-            </tysha-text>
-        </div>
-        <div class="demo-container__item">
-            <tysha-text>
-             in span
-            </tysha-text>
-        </div>
-        <div class="demo-container__item">
-            Theme colors:
-            <div style="background-color: #000">
-              <tysha-text
-                      v-for="(color, i) in colors"
-                      :key="i"
-                      :color="color"
-              >
-              {{color}}
-              </tysha-text>
-            </div>
-        </div>
+  <div class="demo-container">
+    <div class="demo-container__item">
+      <tysha-text tag="div">
+        in div
+      </tysha-text>
     </div>
+    <div class="demo-container__item">
+      <tysha-text>
+        in span
+      </tysha-text>
+    </div>
+    <div class="demo-container__item">
+      Theme colors:
+      <div style="background-color: #000">
+        <tysha-text
+          v-for="(color, i) in colors"
+          :key="i"
+          :color="color"
+        >
+          {{ color }}
+        </tysha-text>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
