a957f89d429a35aa3a70e67a9d48c313bdc87f5c
D3-FC <D3-FC@mail.ru>
2018-09-03 18:48:09 +0400

    feat(TyshaIcon): add icons

---
+++ src/components/Icon/DemoTyshaIcon.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="demo-container">
+        <div class="demo-container__item">
+            <ul style="display: flex; margin: 10px">
+               <li
+                       v-for="(icon, i) in icons"
+                       :key="i"
+               >
+                    <div>
+                      {{icon}}
+                    </div>
+                    <div style="margin-top: 10px; display: flex; justify-content: center">
+                      <tysha-icon :icon="icon"/>
+                    </div>
+               </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import TyshaIcon from './TyshaIcon.vue'
+import { Icon } from './enums/Icon'
+
+export default {
+  components: {
+    TyshaIcon
+  },
+  computed: {
+    icons () {
+      return Object.values(Icon)
+    }
+  }
+}
+</script>
---
+++ src/components/Icon/TyshaIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="tysha-icon">
+    <span
+            class="tysha-icon tysha-icon--ellipsis-v"
+            v-if="icon === iconType.ellipsisV"
+    />
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Icon } from './enums/Icon'
+
+@Component
+export default class TyshaIcon extends Vue {
+  @Prop({
+    validator (v) {
+      return Object.values(Icon).indexOf(v) >= 0
+    }
+  }) icon!: string
+
+  get iconType () {
+    return Icon
+  }
+}
+</script>
+
+<style lang="scss" src="./assets/vendor/icomoon/style.scss"/>
