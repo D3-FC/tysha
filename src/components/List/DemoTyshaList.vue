9d68a55c8aae576ddb38d0f528d15cb1556eacc4
D3-FC <D3-FC@mail.ru>
2018-09-03 15:58:07 +0400

    feat(TyshaList): add overlay

--- src/components/List/DemoTyshaList.vue
+++ src/components/List/DemoTyshaList.vue
@@ -22,6 +22,30 @@
         </tysha-list>
       </div>
     </div>
+    <div
+      v-for="(layer, i) in layers"
+      :key="i"
+      class="demo-container__item"
+      style="background-image: linear-gradient(#d8ffb2, #ffb7e8);"
+    >
+      <div>
+        {{ color }}
+        <tysha-list
+          :layer="layer"
+          color="base"
+        >
+          <tysha-list-item
+            :activated="selected === 0"
+            @click.native="selected = 0">Item 1</tysha-list-item>
+          <tysha-list-item
+            :activated="selected === 1"
+            @click.native="selected = 1">Item 2</tysha-list-item>
+          <tysha-list-item
+            :activated="selected === 2"
+            @click.native="selected = 2">Item 3</tysha-list-item>
+        </tysha-list>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +53,7 @@
 import TyshaList from './TyshaList.vue'
 import TyshaListItem from './TyshaListItem.vue'
 import { ThemeColor } from '../../enums/ThemeColor'
+import { ThemeLayer } from '../../enums/ThemeLayer'
 
 export default {
   components: {
@@ -43,6 +68,9 @@ export default {
   computed: {
     colors () {
       return Object.values(ThemeColor)
+    },
+    layers () {
+      return Object.values(ThemeLayer)
     }
   }
 }
--- src/components/List/TyshaList.vue
+++ src/components/List/TyshaList.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="[colorClassList, sizeClassList]"
+    :class="[colorClassList, sizeClassList, layerClassList]"
     class="tysha-list"
   >
     <slot/>
@@ -12,9 +12,10 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 import { WithColor } from '../../mixins/WithColor'
 import { mixins } from 'vue-class-component'
 import { WithSize } from '../../mixins/WithSize'
+import { WithLayer } from '../../mixins/WithLayer'
 
 @Component
-export default class TyshaList extends mixins(WithColor, WithSize) {
+export default class TyshaList extends mixins(WithColor, WithSize, WithLayer) {
 
 }
 
@@ -24,10 +25,10 @@ export default class TyshaList extends mixins(WithColor, WithSize) {
   .tysha-list {
     background-color: transparent;
     @include tysha-theme--color('background-color');
+    @include tysha-theme--layer();
     border-radius: 1px;
     padding-top: 4px;
     padding-bottom: 4px;
-
     &--color{
       &_primary, &_secondary, &_error, &_success,&_info, &_muted{
         color: $tysha-color--base;
