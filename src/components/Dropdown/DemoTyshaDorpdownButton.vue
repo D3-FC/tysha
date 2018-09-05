18acd91ed2cf865df1cf959f117c2a32a2300061
D3-FC <D3-FC@mail.ru>
2018-09-05 16:51:32 +0400

    feat(TyshaDropdownButton): add button for dropdown

---
+++ src/components/Dropdown/DemoTyshaDorpdownButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="demo-container">
+        <div class="demo-container__item" style="background-color: white">
+            <tysha-dorpdown-button>
+
+            </tysha-dorpdown-button>
+        </div>
+    </div>
+</template>
+
+<script>
+import TyshaDorpdownButton from './TyshaDorpdownButton.vue'
+
+export default {
+  components: {
+    TyshaDorpdownButton
+  }
+}
+</script>
+<style src="../../assets/sass/demo.scss"/>
---
+++ src/components/Dropdown/TyshaDorpdownButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <tysha-button color="base" size="xs" class="tysha-dorpdown-button">
+    <tysha-icon icon="ellipsis-v" class="tysha-dorpdown-button__icon"/>
+  </tysha-button>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import TyshaIcon from '../Icon/TyshaIcon.vue'
+import TyshaButton from '../Button/TyshaButton'
+
+@Component({
+  components: {TyshaButton, TyshaIcon}
+  })
+export default class TyshaDorpdownButton extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+  .tysha-dorpdown-button {
+    &__icon{
+      margin-left: -3px;
+      font-size: 18px;
+    }
+  }
+</style>
