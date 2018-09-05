67c6f25a879a11b6ba9c9e4141015d45989cbfb8
D3-FC <D3-FC@mail.ru>
2018-09-05 17:22:48 +0400

    feat(TyshaBoard): add demo

---
+++ src/components/Board/DemoTyshaBoard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="demo-container">
+        <div class="demo-container__item" style="background-color: white">
+            <tysha-board style="height: 150px; width: 200px" src="https://images.pexels.com/photos/614503/pexels-photo-614503.jpeg?auto=compress&cs=tinysrgb&h=200">
+              asd
+            </tysha-board>
+        </div>
+    </div>
+</template>
+
+<script>
+import TyshaBoard from './TyshaBoard.vue'
+
+export default {
+  components: {
+    TyshaBoard
+  }
+}
+</script>
