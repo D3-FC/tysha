a5a9b12120e0a96e74e2341e6faa50beb6b5e115
D3-FC <D3-FC@mail.ru>
2018-09-05 14:37:01 +0400

    feat(TyshaButton):  add button

---
+++ src/components/Button/DemoTyshaButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="demo-container">
+        <div class="demo-container__item" style="background-color: white">
+            <div v-for="(color, i) in colors" :key="i" style="margin: 10px;">
+              {{color}}
+
+              <div style=" border: 1px solid black">
+                <tysha-button  :color="color">
+                +
+                </tysha-button>
+              </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TyshaButton from './TyshaButton.vue'
+import { ThemeColor } from '../../enums/ThemeColor'
+
+export default {
+  components: {
+    TyshaButton
+  },
+  computed: {
+    colors () {
+      return Object.values(ThemeColor)
+    }
+  }
+}
+</script>
---
+++ src/components/Button/TyshaButton.vue
@@ -0,0 +1,120 @@
+<template>
+  <button class="tysha-button" :class="[sizeClassList, colorClassList]">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { WithSize } from '../../mixins/WithSize'
+import { mixins } from 'vue-class-component'
+import { WithColor } from '../../mixins/WithColor'
+
+@Component
+export default class TyshaButton extends mixins(WithSize, WithColor) {
+
+}
+</script>
+
+<style lang="scss">
+  %tysha-button{
+    &:active{
+      @include tysha-shadow--active;
+    }
+  }
+
+  .tysha-button {
+    @include reset-button;
+    $shift: 8px;
+    width: calc(100% + #{$shift});
+    @include tysha-shadow;
+    @include tysha-transition(background-color, color, box-shadow);
+
+    &--size {
+      &_lg {
+        padding: 13px 20px;
+      }
+      &_md {
+        padding: 10px 17px;
+      }
+      &_sm {
+        padding: 6px 10px;
+      }
+      &_xs {
+        padding: 3px 5px;
+      }
+    }
+    @include tysha-theme--size__font;
+
+    &--color {
+      &_base {
+        background-color: $tysha-color--base;
+        color: $tysha-color--secondary;
+
+        &:hover{
+          background-color: lighten($tysha-color--base, 10%);
+          color: lighten($tysha-color--secondary, 10%) ;
+        }
+        @extend %tysha-button;
+      }
+      &_primary {
+        background-color: $tysha-color--primary;
+        color: $tysha-color--base;
+
+        &:hover{
+          background-color: lighten($tysha-color--primary, 20%);
+          color: darken($tysha-color--base, 20%) ;
+        }
+        @extend %tysha-button;
+      }
+      &_secondary {
+        background-color: $tysha-color--secondary;
+        color: $tysha-color--base;
+
+        &:hover{
+          background-color: lighten($tysha-color--secondary, 5%);
+          color: darken($tysha-color--base, 20%) ;
+        }
+        @extend %tysha-button;
+      }
+      &_error {
+        background-color: $tysha-color--error;
+        color: $tysha-color--white;
+
+        &:hover{
+          background-color: lighten($tysha-color--error, 5%);
+        }
+        @extend %tysha-button;
+      }
+      &_success {
+        background-color: $tysha-color--success;
+        color: $tysha-color--base;
+
+        &:hover{
+          background-color: lighten($tysha-color--success, 20%);
+          color: darken($tysha-color--base, 20%) ;
+        }
+        @extend %tysha-button;
+      }
+      &_info {
+        background-color: $tysha-color--info;
+        color: $tysha-color--base;
+
+        &:hover{
+          background-color: lighten($tysha-color--info, 20%);
+          color: darken($tysha-color--base, 20%) ;
+        }
+        @extend %tysha-button;
+      }
+      &_muted {
+        background-color: $tysha-color--muted;
+        color: $tysha-color--white;
+
+        &:hover{
+          background-color: lighten($tysha-color--muted, 10%);
+        }
+        @extend %tysha-button;
+      }
+    }
+  }
+</style>
